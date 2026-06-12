6cbde164dcefa3da442bbecec4d83ba0cd8d2c38
Sagar Ahirrao <sagarahirrao111@gmail.com>
2026-06-12 14:06:27 +0530

    Created Path Parameters

--- main.py
+++ main.py
@@ -24,4 +24,11 @@ def view():
     
     return data
     
-    
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str):
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+
+    return {'error': 'patient not found'}
