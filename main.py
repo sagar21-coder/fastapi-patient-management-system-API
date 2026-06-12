e846d72639e0bc1d632336a648c0395b8244b440
Sagar Ahirrao <sagarahirrao111@gmail.com>
2026-06-12 16:23:56 +0530

    added path function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 import json
 
 app = FastAPI()
@@ -25,7 +25,7 @@ def view():
     return data
     
 @app.get('/patient/{patient_id}')
-def view_patient(patient_id: str):
+def view_patient(patient_id: str = Path(..., desription='ID of the patient in the DB', example='P001')):
     data = load_data()
 
     if patient_id in data:
