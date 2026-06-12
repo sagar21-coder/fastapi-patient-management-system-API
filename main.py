8d256db8ef46cbaed5b6026519ace0200dd41d2b
Sagar Ahirrao <sagarahirrao111@gmail.com>
2026-06-12 13:06:04 +0530

    Loaded json data into progrm

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import json
 
 app = FastAPI()
 
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+        
+        return data
+
+
 @app.get("/")
 def hello():
     return{'message':'Patient Management System API'}
@@ -10,4 +17,11 @@ def hello():
 @app.get("/about")
 def about():
     return{'message':'A fully functional API to manage your patients records'}
+    
+@app.get('/view')
+def view():
+    data = load_data()
+    
+    return data
+    
     
