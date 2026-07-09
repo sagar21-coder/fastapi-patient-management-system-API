aa1f32215467def49b87cc15ab2e496df4db2ab7
Sagar Ahirrao <sagarahirrao111@gmail.com>
2026-07-09 14:50:13 +0530

    update and delete

--- main.py
+++ main.py
@@ -114,3 +114,49 @@ def create_patient(patient: Patient):
     save_data(data)
 
     return JSONResponse(status_code=201, content={'message':'patient created successfully'})
+
+
+@app.put('/edit/{patient_id}')
+def update_patient(patient_id: str, patient_update: PatientUpdate):
+
+    data = load_data()
+    
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail='Patient not found')
+    
+    existing_patient_info = data[patient_id]
+    
+    updated_patient_info = patient_update.model_dump(exclude_unset=True)
+    
+    for key, value in updated_patient_info.items():
+        existing_patient_info[key] = value
+        
+    # existing_patient_info -> pydantic object -> updated bmi + verdict
+    existing_patient_info['id'] = patient_id
+    patient_pydantic_obj = Patient(**existing_patient_info)
+    
+    # -> pydantic object -> dict    
+    existing_patient_info = patient_pydantic_obj.model_dump(exclude='id')
+        
+    # add this dict to data
+    data[patient_id] = existing_patient_info   
+    
+    # save data 
+    save_data(data)
+    
+    return JSONResponse(status_code=200, content={'message':'patient updated'})
+
+@app.delete('/delete/{patient_id}')
+def delete_patient(patient_id: str):
+    
+    # load data
+    data = load_data()
+    
+    if patient_id not in data:
+      raise HTTPException(status_code=404, detail='Patient not found')
+
+    del data[patient_id]
+    
+    save_data(data)
+    
+    return JSONResponse(status_code=200, content={'message': 'patient deleted'})
